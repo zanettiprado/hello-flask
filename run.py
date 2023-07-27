34cbab4a1f57eb6f2b178c79d4fe43003dff205b
zanettiprado <zanettiprado@gmail.com>
2023-07-27 17:07:14 +0000

    add message and flash to our contact page

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -31,8 +34,12 @@ def about_member(member_name):
     
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, We have received your message!".format(
+            request.form.get("name")))
+    
     return render_template("contact.html", page_title="Contact")
 
 @app.route("/careers")
